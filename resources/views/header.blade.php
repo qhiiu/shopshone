912d5878b73bc4d0c66cb4ad6bba19c15b6a0254
qujhiiu <qujhiiu@gmail.com>
2019-09-01 09:51:00 +0700

    a

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -63,7 +63,7 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand">9x shop </a>
+            <a class="navbar-brand"></a>
           </div>
           <div class="collapse navbar-collapse" id="myNavbar">
             <ul class="nav navbar-nav">
